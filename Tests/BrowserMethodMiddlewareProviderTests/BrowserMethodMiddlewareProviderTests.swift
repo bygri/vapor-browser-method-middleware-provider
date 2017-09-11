e863595bc9ce1c409616b9b797c871855a8f887a
Toby Griffin <toby.griffin@gmail.com>
2017-09-12 09:10:32 +1000

    More comprehensive test

--- Tests/BrowserMethodMiddlewareProviderTests/BrowserMethodMiddlewareProviderTests.swift
+++ Tests/BrowserMethodMiddlewareProviderTests/BrowserMethodMiddlewareProviderTests.swift
@@ -1,7 +1,8 @@
 import XCTest
-import BrowserMethodMiddlewareProvider
+@testable import BrowserMethodMiddlewareProvider
 import Testing
 import Vapor
+import HTTP
 
 class BrowserMethodMiddlewareProviderTests: XCTestCase {
 
@@ -32,10 +33,38 @@ class BrowserMethodMiddlewareProviderTests: XCTestCase {
     XCTAssertTrue(patchCalled)
   }
 
+  func testCommonVerb() throws {
+    let middleware = BrowserMethodMiddleware()
+    let finalResponder = BasicResponder() { req in return Response(status: .ok) }
+    let req = Request.makeTest(method: .post, path: "/")
+    req.formURLEncoded = ["_method": "get"]
+    let _ = try middleware.respond(to: req, chainingTo: finalResponder)
+    XCTAssertTrue(req.method == .get)
+  }
+
+  func testUncommonVerb() throws {
+    let middleware = BrowserMethodMiddleware()
+    let finalResponder = BasicResponder() { req in return Response(status: .ok) }
+    let req = Request.makeTest(method: .post, path: "/")
+    req.formURLEncoded = ["_method": "teapot"]
+    let _ = try middleware.respond(to: req, chainingTo: finalResponder)
+    // Ensure it hasn't changed
+    print("The method is \(req.method)")
+    guard
+      case let .other(methodName) = req.method,
+      methodName == "TEAPOT"
+     else {
+      XCTFail()
+      return
+    }
+  }
+
 }
 
 extension BrowserMethodMiddlewareProviderTests {
   static let allTests = [
     ("testProvider", testProvider),
+    ("testCommonVerb", testCommonVerb),
+    ("testUncommonVerb", testUncommonVerb),
   ]
 }
